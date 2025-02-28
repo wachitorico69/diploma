835c00a3fbe4f92bb1f54c271fd96b1edc0e1dd4
wachitorico69 <morenodario919@gmail.com>
2025-02-28 11:53:24 -0600

    whatever

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,9 @@ function guardarDatos() {
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const img1 = document.getElementById("uaa");
+const img2 = document.getElementById("ccbas");
+const img3 = document.getElementById("firma");
 
-ctx.fillStyle = "white";
-ctx.fillRect(10, 10, 150, 100);
+ctx.drawImage(img1, 80, 10, 160, 155);
+ctx.drawImage(img2, 600, -10, 315, 190);
