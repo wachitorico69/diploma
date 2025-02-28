f435f6a5c11e1a27d4c41dfe0d2aaae24296079e
wachitorico69 <morenodario919@gmail.com>
2025-02-28 10:38:00 -0600

    smth

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,9 @@ function guardarDatos() {
 
     window.location.href = "diploma.html"
 }
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "white";
+ctx.fillRect(10, 10, 150, 100);
